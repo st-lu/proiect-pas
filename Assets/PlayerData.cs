5c9c0140af1d8ad6869ba6401aa04ca07eb304d1
Diana Poinarita <61639111+PoinaritaDiana@users.noreply.github.com>
2022-05-19 22:57:36 +0300

    Add player data with health and position

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float health;
+    public float[] position;
+
+    public PlayerData (Player player)
+    {
+        health = player.currentHealth;
+
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
