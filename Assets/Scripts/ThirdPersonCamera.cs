db803ec1d34aba97b61ed3eddf64371f1bc13693
Agha Mara <62206596+maradarin@users.noreply.github.com>
2022-05-21 04:13:50 +0300

    Create ThirdPersonCamera.cs

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform target, player;
+    public Transform camTransform;
+    
+    private void Start()
+    {
+        camTransform = transform;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+    }
+}
